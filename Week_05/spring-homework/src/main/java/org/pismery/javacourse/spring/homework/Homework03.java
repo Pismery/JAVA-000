13c3f55aee3924684a30f1e5ee66e8553787668b
pismery <pismery@163.com>
2020-11-22 16:20:39 +0800

    Week 5 Homework: update JDBC operation to unit test

--- Week_05/spring-homework/src/main/java/org/pismery/javacourse/spring/homework/Homework03.java
+++ Week_05/spring-homework/src/main/java/org/pismery/javacourse/spring/homework/Homework03.java
@@ -4,195 +4,88 @@ import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
 import java.sql.*;
-import java.util.function.Function;
-import java.util.stream.IntStream;
+import java.util.List;
 
 /**
- * Homework for JDBC operation
+ * Homework for JDBC operation, Operation see {@link org.pismery.javacourse.spring.homework.Homework03Test}
  */
 public class Homework03 {
 
-    public static final String JDBC_URL = "jdbc:mysql://localhost/test?serverTimezone=GMT";
-    public static final String USER_NAME = "root";
-    public static final String PWD = "123123";
+    static class ConnectionManager {
+        private static final String JDBC_URL = "jdbc:mysql://localhost/test?serverTimezone=GMT";
+        private static final String USER_NAME = "root";
+        private static final String PWD = "123123";
+        private static final HikariDataSource ds;
 
-    public static void main(String[] args) {
-        System.out.println("jdbcCRUD");
-        jdbcCRUD();
+        static {
+            HikariConfig config = new HikariConfig();
+            config.setJdbcUrl(JDBC_URL); //数据源
+            config.setUsername(USER_NAME); //用户名
+            config.setPassword(PWD); //密码
+            config.addDataSourceProperty("cachePrepStmts", "true"); //是否自定义配置，为true时下面两个参数才生效
+            config.addDataSourceProperty("prepStmtCacheSize", "250"); //连接池大小默认25，官方推荐250-500
+            config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048"); //单条语句最大长度默认256，官方推荐2048
+            config.addDataSourceProperty("useServerPrepStmts", "true"); //新版本MySQL支持服务器端准备，开启能够得到显著性能提升
+            config.addDataSourceProperty("useLocalSessionState", "true");
+            config.addDataSourceProperty("useLocalTransactionState", "true");
+            config.addDataSourceProperty("rewriteBatchedStatements", "true");
+            config.addDataSourceProperty("cacheResultSetMetadata", "true");
+            config.addDataSourceProperty("cacheServerConfiguration", "true");
+            config.addDataSourceProperty("elideSetAutoCommits", "true");
+            config.addDataSourceProperty("maintainTimeStats", "false");
 
-        System.out.println("jdbcCRUDWithPrepareStatement");
-        jdbcCRUDWithPrepareStatement();
-
-        System.out.println("jdbcCRUDWithBatch");
-        jdbcCRUDWithBatch();
-
-        System.out.println("jdbcCRUDWithHikari");
-        jdbcCRUDWithHikari();
-    }
-
-    private static void jdbcCRUDWithHikari() {
-        HikariDataSource ds = configHikariDataSource();
-
-        try (Connection connection = ds.getConnection()) {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            System.out.println("Init");
-            String deleteSQL = "delete from t";
-            //execute(connection, deleteSQL);
-            //executeQuery(connection,"select * from t");
-            int[] totalRows = null;
-            String insertSQL = "insert into t(id,c,d) values (?,?,?)";
-            totalRows = executeBatch(connection, insertSQL
-                    ,i -> i
-                    ,i -> i
-                    ,i -> i
-            );
-            System.out.println("Insert totalRows:" + IntStream.of(totalRows).sum());
-
-            String updateSQL = "update t set d = ? where id = ?";
-            totalRows = executeBatch(connection, updateSQL
-                    ,i -> i + 1
-                    ,i -> i
-            );
-            System.out.println("Update totalRows:" + IntStream.of(totalRows).sum());
-
-            System.out.println("Delete SQL:"+execute(connection, deleteSQL));
-        } catch (SQLException | ClassNotFoundException e) {
-            e.printStackTrace();
+            ds = new HikariDataSource(config);
         }
-    }
 
-    private static HikariDataSource configHikariDataSource() {
-        HikariConfig config = new HikariConfig();
-        config.setJdbcUrl(JDBC_URL); //数据源
-        config.setUsername(USER_NAME); //用户名
-        config.setPassword(PWD); //密码
-        config.addDataSourceProperty("cachePrepStmts", "true"); //是否自定义配置，为true时下面两个参数才生效
-        config.addDataSourceProperty("prepStmtCacheSize", "250"); //连接池大小默认25，官方推荐250-500
-        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048"); //单条语句最大长度默认256，官方推荐2048
-        config.addDataSourceProperty("useServerPrepStmts", "true"); //新版本MySQL支持服务器端准备，开启能够得到显著性能提升
-        config.addDataSourceProperty("useLocalSessionState", "true");
-        config.addDataSourceProperty("useLocalTransactionState", "true");
-        config.addDataSourceProperty("rewriteBatchedStatements", "true");
-        config.addDataSourceProperty("cacheResultSetMetadata", "true");
-        config.addDataSourceProperty("cacheServerConfiguration", "true");
-        config.addDataSourceProperty("elideSetAutoCommits", "true");
-        config.addDataSourceProperty("maintainTimeStats", "false");
 
-        return new HikariDataSource(config);
-    }
-
-    private static void jdbcCRUDWithBatch() {
-        try (Connection connection = DriverManager.getConnection(JDBC_URL, USER_NAME, PWD)) {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            System.out.println("Init");
-            String deleteSQL = "delete from t";
-            execute(connection, deleteSQL);
-            executeQuery(connection,"select * from t");
-
-            String insertSQL = "insert into t(id,c,d) values (?,?,?)";
-            int[] totalRows = executeBatch(connection, insertSQL
-                    ,i -> i
-                    ,i -> i
-                    ,i -> i
-            );
-            System.out.println("Insert totalRows:" + IntStream.of(totalRows).sum());
-
-            String updateSQL = "update t set d = ? where id = ?";
-            totalRows = executeBatch(connection, updateSQL
-                    ,i -> i + 1
-                    ,i -> i
-            );
-            System.out.println("Update totalRows:" + IntStream.of(totalRows).sum());
-
-            System.out.println("Delete SQL:"+execute(connection, deleteSQL));
-        } catch (SQLException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static void jdbcCRUDWithPrepareStatement() {
-        try (Connection connection = DriverManager.getConnection(JDBC_URL, USER_NAME, PWD)) {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            String selectById = "select id,c,d from t where id = ?";
-            String deleteSQL = "delete from t where id = ?";
-            String insertSQL = "insert into t(id,c,d) values (?,?,?)";
-            String updateSQL = "update t set c = ? where id = ?";
-
-            System.out.println("Init");
-            execute(connection, deleteSQL, 1);
-            executeQuery(connection, selectById, 1);
-
-            System.out.print("Insert SQL: ");
-            System.out.println(execute(connection, insertSQL, 1, 1, 1));
-
-            System.out.print("Update SQL: ");
-            System.out.println(execute(connection, updateSQL, 2, 1));
-
-            System.out.print("Delete SQL: ");
-            System.out.println(execute(connection, deleteSQL, 1));
-
-        } catch (SQLException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static void jdbcCRUD() {
-        try (Connection connection = DriverManager.getConnection(JDBC_URL, USER_NAME, PWD);
-             Statement statement = connection.createStatement()
-        ) {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            String selectAll = "select id,c,d from t;";
-            String insertSQL = "insert into t(id,c,d) values (1,1,1)";
-            String updateSQL = "update t set c = 2 where id = 1";
-            String deleteSQL = "delete from t where id = 1";
-
-            System.out.println("Init");
-            statement.executeUpdate(deleteSQL);
-            executeQuery(statement, selectAll);
-
-            System.out.print("Insert SQL: ");
-            System.out.println(statement.executeUpdate(insertSQL));
-
-            System.out.print("Update SQL: ");
-            System.out.println(statement.executeUpdate(updateSQL));
-
-            System.out.print("Delete SQL: ");
-            System.out.println(statement.executeUpdate(deleteSQL));
-
-        } catch (SQLException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static void executeQuery(Statement statement, String sql) throws SQLException {
-        try (ResultSet resultSet = statement.executeQuery(sql)) {
-            while (resultSet.next()) {
-                System.out.println(String.format("{id:%s, c:%s, d:%s}", resultSet.getInt("id"),
-                        resultSet.getString("c"),
-                        resultSet.getString("d")
-                ));
+        static Connection getConnectionFromDriver() {
+            try {
+                return DriverManager.getConnection(JDBC_URL, USER_NAME, PWD);
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
+            return null;
+        }
+
+        static Connection getConnectionFromPool() {
+            try {
+                return ds.getConnection();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return null;
         }
     }
 
-    private static void executeQuery(Connection conn, String sql, Object... args) throws SQLException {
+    public void executeQuery(Connection conn, String sql) {
+        try (Statement statement = conn.createStatement()) {
+            try (ResultSet resultSet = statement.executeQuery(sql)) {
+                printResultSet(resultSet);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int execute(Connection conn, String sql) throws SQLException {
+        try (Statement statement = conn.createStatement()) {
+            return statement.executeUpdate(sql);
+        }
+    }
+
+    public void executeQuery(Connection conn, String sql, Object... args) throws SQLException {
         try (PreparedStatement statement = conn.prepareStatement(sql)) {
-            for (int i = 1; i <= args.length; i++) {
-                statement.setObject(i, args[i - 1]);
+            for (int i = 0; i < args.length; i++) {
+                statement.setObject(i + 1, args[i]);
             }
+
             try (ResultSet resultSet = statement.executeQuery()) {
-                while (resultSet.next()) {
-                    System.out.println(String.format("{id:%s, c:%s, d:%s}",
-                            resultSet.getInt("id"),
-                            resultSet.getString("c"),
-                            resultSet.getString("d")
-                    ));
-                }
+                printResultSet(resultSet);
             }
         }
     }
 
-    private static int execute(Connection conn, String sql, Object... args) throws SQLException {
+    public int execute(Connection conn, String sql, Object... args) throws SQLException {
         try (PreparedStatement statement = conn.prepareStatement(sql)) {
             for (int i = 1; i <= args.length; i++) {
                 statement.setObject(i, args[i - 1]);
@@ -202,23 +95,32 @@ public class Homework03 {
         }
     }
 
-    @SafeVarargs
-    private static int[] executeBatch(Connection conn, String sql, Function<Integer, Object>... functions) {
-        int[] totalRows = null;
+    public int[] executeBatch(Connection conn, String sql, List<List<Object>> argsList) throws SQLException {
         try (PreparedStatement statement = conn.prepareStatement(sql)) {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-
-            for (int i = 1; i < 100; i++) {
-                for (int j = 0; j < functions.length; j++) {
-                    statement.setObject(j + 1, functions[j].apply(i));
+            for (List<Object> objects : argsList) {
+                for (int i = 0; i < objects.size(); i++) {
+                    statement.setObject(i + 1, objects.get(i));
                 }
                 statement.addBatch();
             }
 
-            totalRows = statement.executeBatch();
-        } catch (SQLException | ClassNotFoundException e) {
-            e.printStackTrace();
+            return statement.executeBatch();
         }
-        return totalRows;
+    }
+
+    private void printResultSet(ResultSet resultSet) throws SQLException {
+        boolean empty = true;
+        while (resultSet.next()) {
+            empty = false;
+            System.out.println(String.format("{id:%s, c:%s, d:%s}",
+                    resultSet.getInt("id"),
+                    resultSet.getString("c"),
+                    resultSet.getString("d")
+            ));
+        }
+
+        if (empty) {
+            System.out.println("select nothing...");
+        }
     }
 }
